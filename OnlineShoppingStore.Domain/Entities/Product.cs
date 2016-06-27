a7b6c14cb267e20131ab3d90f7dff50b3042b61d
Rakesh <rakeshmaharjan066@gmail.com>
2016-06-27 17:48:22 +0545

    second one

--- OnlineShoppingStore.Domain/Entities/Product.cs
+++ OnlineShoppingStore.Domain/Entities/Product.cs
@@ -9,6 +9,7 @@ namespace OnlineShoppingStore.Domain.Entities
     public class Product
     {
         public int ProductId { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
--- OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,37 @@ using System.Web;
 using System.Web.Mvc;
 using OnlineShoppingStore.Domain.Abstract;
 using OnlineShoppingStore.Domain.Entities;
+using OnlineShoppingStore.WebUI.Models;
+using OnlineShoppingStore.WebUI.HtmlHelpers;
 
 namespace OnlineShoppingStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductRepository repository;
-
+        public int PageSize = 4;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
         // GET: Product
-        public ViewResult List()
+        public ViewResult List(int page =1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
